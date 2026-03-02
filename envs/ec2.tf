07acacd411d9171252ebf23617377ef70aba6b03
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-02 20:23:46 +0900

    :recycle: 共通キーペア作成

---
+++ envs/ec2.tf
@@ -0,0 +1,21 @@
+/************************************************************
+KeyPair
+************************************************************/
+resource "tls_private_key" "ssh_keygen" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_sensitive_file" "keypair_pem" {
+  filename        = "${path.module}/.key/keypair.pem"
+  content         = tls_private_key.ssh_keygen.private_key_pem
+  file_permission = "0600"
+}
+
+resource "aws_key_pair" "keypair" {
+  key_name   = "common-keypair"
+  public_key = tls_private_key.ssh_keygen.public_key_openssh
+  tags = {
+    Name = "common-keypair"
+  }
+}
