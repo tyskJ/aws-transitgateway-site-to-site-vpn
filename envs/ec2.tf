8a4ab1cb6d2e8122ccc6ee12b21c10fb8acc10c8
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-09 09:37:22 +0000

    :memo: 各EC2の設定ファイル分割を見越して定義を分割

--- envs/ec2.tf
+++ envs/ec2.tf
@@ -63,15 +63,13 @@ resource "aws_eip" "this" {
 /************************************************************
 EC2 - Gateway
 ************************************************************/
-resource "aws_instance" "gateway" {
-  for_each = local.instances
-
+resource "aws_instance" "gateway_a" {
   ami           = data.aws_ami_ids.ubuntu.ids[0]
   key_name      = aws_key_pair.keypair.id
   instance_type = "c6i.large"
-  subnet_id     = aws_subnet.this[each.value.subnet_key].id
+  subnet_id     = aws_subnet.this["${local.instances.onpremises_gateway_ec2_a.subnet_key}"].id
   vpc_security_group_ids = [
-    aws_security_group.this[each.value.sg_key].id
+    aws_security_group.this["${local.instances.onpremises_gateway_ec2_a.sg_key}"].id
   ]
   ebs_optimized = true
   root_block_device {
@@ -82,7 +80,7 @@ resource "aws_instance" "gateway" {
     delete_on_termination = true
     encrypted             = true
     tags = {
-      Name = "${each.value.name}-root-volume"
+      Name = "${local.instances.onpremises_gateway_ec2_a.name}-root-volume"
     }
   }
   metadata_options {
@@ -94,37 +92,37 @@ resource "aws_instance" "gateway" {
   disable_api_stop        = false
   disable_api_termination = false
   force_destroy           = true
-  iam_instance_profile    = aws_iam_instance_profile.this[each.value.instanceprofile_key].name
+  iam_instance_profile    = aws_iam_instance_profile.this["${local.instances.onpremises_gateway_ec2_a.instanceprofile_key}"].name
   source_dest_check       = false
   user_data_base64 = base64gzip(
     templatefile("${path.module}/config/setup.sh", {
       nw_conf = file("${path.module}/config/99-vpn.conf")
       xfrm_conf = templatefile("${path.module}/config/xfrm-ifaces.service", {
-        cgwside_tunnel1_insideip = aws_vpn_connection.this[each.key].tunnel1_cgw_inside_address
-        cgwside_tunnel2_insideip = aws_vpn_connection.this[each.key].tunnel2_cgw_inside_address
+        cgwside_tunnel1_insideip = aws_vpn_connection.this["onpremises_gateway_ec2_a"].tunnel1_cgw_inside_address
+        cgwside_tunnel2_insideip = aws_vpn_connection.this["onpremises_gateway_ec2_a"].tunnel2_cgw_inside_address
       })
-      cgw_gip             = aws_eip.this[each.key].public_ip
-      awsside_tunnel1_gip = aws_vpn_connection.this[each.key].tunnel1_address
-      awsside_tunnel2_gip = aws_vpn_connection.this[each.key].tunnel2_address
-      tunnel1_psk         = aws_vpn_connection.this[each.key].tunnel1_preshared_key
-      tunnel2_psk         = aws_vpn_connection.this[each.key].tunnel2_preshared_key
+      cgw_gip             = aws_eip.this["onpremises_gateway_ec2_a"].public_ip
+      awsside_tunnel1_gip = aws_vpn_connection.this["onpremises_gateway_ec2_a"].tunnel1_address
+      awsside_tunnel2_gip = aws_vpn_connection.this["onpremises_gateway_ec2_a"].tunnel2_address
+      tunnel1_psk         = aws_vpn_connection.this["onpremises_gateway_ec2_a"].tunnel1_preshared_key
+      tunnel2_psk         = aws_vpn_connection.this["onpremises_gateway_ec2_a"].tunnel2_preshared_key
       charon_conf         = file("${path.module}/config/add-charon.conf")
       ens6_conf = templatefile("${path.module}/config/20-ens6.network", {
         onpremises_client_private_a_subnet_cidr           = local.subnets.onpremises_client_private_a.cidr
-        onpremises_gateway_private_a_subnet_vpc_router_ip = cidrhost(local.subnets.onpremises_gateway_public_a.cidr, 1)
+        onpremises_gateway_private_subnet_vpc_router_ip = cidrhost(local.subnets.onpremises_gateway_public_a.cidr, 1)
         aws_vpc_cidr                                      = local.vpcs.aws.cidr
       })
       rtbrule_conf = templatefile("${path.module}/config/tgw-ecmp.service", {
         aws_vpc_cidr = local.vpcs.aws.cidr
       })
-      frr_conf = templatefile("${path.module}/config/frr_${each.key}.conf", {
+      frr_conf = templatefile("${path.module}/config/frr_onpremises_gateway_ec2_a.conf", {
         cgw_asn                  = 65000
-        cgw_gip                  = aws_eip.this[each.key].public_ip
-        cgwside_tunnel1_insideip = aws_vpn_connection.this[each.key].tunnel1_cgw_inside_address
-        cgwside_tunnel2_insideip = aws_vpn_connection.this[each.key].tunnel2_cgw_inside_address
+        cgw_gip                  = aws_eip.this["onpremises_gateway_ec2_a"].public_ip
+        cgwside_tunnel1_insideip = aws_vpn_connection.this["onpremises_gateway_ec2_a"].tunnel1_cgw_inside_address
+        cgwside_tunnel2_insideip = aws_vpn_connection.this["onpremises_gateway_ec2_a"].tunnel2_cgw_inside_address
         aws_tgw_asn              = 64512
-        awsside_tunnel1_insideip = aws_vpn_connection.this[each.key].tunnel1_vgw_inside_address
-        awsside_tunnel2_insideip = aws_vpn_connection.this[each.key].tunnel2_vgw_inside_address
+        awsside_tunnel1_insideip = aws_vpn_connection.this["onpremises_gateway_ec2_a"].tunnel1_vgw_inside_address
+        awsside_tunnel2_insideip = aws_vpn_connection.this["onpremises_gateway_ec2_a"].tunnel2_vgw_inside_address
         onpremises_nw_cidr       = local.vpcs.onpremises.cidr
       })
       snat_conf = templatefile("${path.module}/config/snat_sources.conf", {
@@ -135,7 +133,88 @@ resource "aws_instance" "gateway" {
     })
   )
   tags = {
-    Name = each.value.name
+    Name = local.instances.onpremises_gateway_ec2_a.name
+  }
+  # userdataを変更すると再起動が走るため抑止
+  # 代わりに、user_data_replace_on_change は効かなくなる
+  lifecycle {
+    ignore_changes = [
+      user_data_base64
+    ]
+  }
+}
+
+resource "aws_instance" "gateway_c" {
+  ami           = data.aws_ami_ids.ubuntu.ids[0]
+  key_name      = aws_key_pair.keypair.id
+  instance_type = "c6i.large"
+  subnet_id     = aws_subnet.this["${local.instances.onpremises_gateway_ec2_c.subnet_key}"].id
+  vpc_security_group_ids = [
+    aws_security_group.this["${local.instances.onpremises_gateway_ec2_c.sg_key}"].id
+  ]
+  ebs_optimized = true
+  root_block_device {
+    volume_size           = 100
+    volume_type           = "gp3"
+    iops                  = 3000
+    throughput            = 125
+    delete_on_termination = true
+    encrypted             = true
+    tags = {
+      Name = "${local.instances.onpremises_gateway_ec2_c.name}-root-volume"
+    }
+  }
+  metadata_options {
+    http_tokens = "required"
+  }
+  maintenance_options {
+    auto_recovery = "default"
+  }
+  disable_api_stop        = false
+  disable_api_termination = false
+  force_destroy           = true
+  iam_instance_profile    = aws_iam_instance_profile.this["${local.instances.onpremises_gateway_ec2_c.instanceprofile_key}"].name
+  source_dest_check       = false
+  user_data_base64 = base64gzip(
+    templatefile("${path.module}/config/setup.sh", {
+      nw_conf = file("${path.module}/config/99-vpn.conf")
+      xfrm_conf = templatefile("${path.module}/config/xfrm-ifaces.service", {
+        cgwside_tunnel1_insideip = aws_vpn_connection.this["onpremises_gateway_ec2_c"].tunnel1_cgw_inside_address
+        cgwside_tunnel2_insideip = aws_vpn_connection.this["onpremises_gateway_ec2_c"].tunnel2_cgw_inside_address
+      })
+      cgw_gip             = aws_eip.this["onpremises_gateway_ec2_c"].public_ip
+      awsside_tunnel1_gip = aws_vpn_connection.this["onpremises_gateway_ec2_c"].tunnel1_address
+      awsside_tunnel2_gip = aws_vpn_connection.this["onpremises_gateway_ec2_c"].tunnel2_address
+      tunnel1_psk         = aws_vpn_connection.this["onpremises_gateway_ec2_c"].tunnel1_preshared_key
+      tunnel2_psk         = aws_vpn_connection.this["onpremises_gateway_ec2_c"].tunnel2_preshared_key
+      charon_conf         = file("${path.module}/config/add-charon.conf")
+      ens6_conf = templatefile("${path.module}/config/20-ens6.network", {
+        onpremises_client_private_a_subnet_cidr           = local.subnets.onpremises_client_private_a.cidr
+        onpremises_gateway_private_subnet_vpc_router_ip = cidrhost(local.subnets.onpremises_gateway_public_c.cidr, 1)
+        aws_vpc_cidr                                      = local.vpcs.aws.cidr
+      })
+      rtbrule_conf = templatefile("${path.module}/config/tgw-ecmp.service", {
+        aws_vpc_cidr = local.vpcs.aws.cidr
+      })
+      frr_conf = templatefile("${path.module}/config/frr_onpremises_gateway_ec2_c.conf", {
+        cgw_asn                  = 65000
+        cgw_gip                  = aws_eip.this["onpremises_gateway_ec2_c"].public_ip
+        cgwside_tunnel1_insideip = aws_vpn_connection.this["onpremises_gateway_ec2_c"].tunnel1_cgw_inside_address
+        cgwside_tunnel2_insideip = aws_vpn_connection.this["onpremises_gateway_ec2_c"].tunnel2_cgw_inside_address
+        aws_tgw_asn              = 64512
+        awsside_tunnel1_insideip = aws_vpn_connection.this["onpremises_gateway_ec2_c"].tunnel1_vgw_inside_address
+        awsside_tunnel2_insideip = aws_vpn_connection.this["onpremises_gateway_ec2_c"].tunnel2_vgw_inside_address
+        onpremises_nw_cidr       = local.vpcs.onpremises.cidr
+      })
+      snat_conf = templatefile("${path.module}/config/snat_sources.conf", {
+        aws_client_private_a_subnet_cidr        = local.subnets.aws_client_private_a.cidr
+        onpremises_client_private_a_subnet_cidr = local.subnets.onpremises_client_private_a.cidr
+      })
+      ipset_conf = file("${path.module}/config/nat-ens5-ipset.service")
+    })
+  )
+  tags = {
+    Name = local.instances.onpremises_gateway_ec2_c.name
   }
   # userdataを変更すると再起動が走るため抑止
   # 代わりに、user_data_replace_on_change は効かなくなる
@@ -149,21 +228,31 @@ resource "aws_instance" "gateway" {
 /************************************************************
 Elastice IP Association
 ************************************************************/
-resource "aws_eip_association" "this" {
-  for_each = local.eips
+resource "aws_eip_association" "gateway_a" {
+  instance_id   = aws_instance.gateway_a.id
+  allocation_id = aws_eip.this["onpremises_gateway_ec2_a"].allocation_id
+}
 
-  instance_id   = aws_instance.gateway[each.value.instance_key].id
-  allocation_id = aws_eip.this[each.key].allocation_id
+resource "aws_eip_association" "gateway_c" {
+  instance_id   = aws_instance.gateway_c.id
+  allocation_id = aws_eip.this["onpremises_gateway_ec2_c"].allocation_id
 }
 
 /************************************************************
 Secondary ENI
 ************************************************************/
-resource "aws_network_interface_attachment" "this" {
-  for_each   = local.instances
-  depends_on = [aws_eip_association.this]
+resource "aws_network_interface_attachment" "gateway_a" {
+  depends_on = [aws_eip_association.gateway_a]
 
-  instance_id          = aws_instance.gateway[each.key].id
-  network_interface_id = aws_network_interface.this[each.value.secondary_eni_key].id
+  instance_id          = aws_instance.gateway_a.id
+  network_interface_id = aws_network_interface.this["${local.instances.onpremises_gateway_ec2_a.secondary_eni_key}"].id
+  device_index         = 1
+}
+
+resource "aws_network_interface_attachment" "gateway_c" {
+  depends_on = [aws_eip_association.gateway_c]
+
+  instance_id          = aws_instance.gateway_c.id
+  network_interface_id = aws_network_interface.this["${local.instances.onpremises_gateway_ec2_c.secondary_eni_key}"].id
   device_index         = 1
 }
--- envs/outputs.tf
+++ envs/outputs.tf
@@ -1,6 +1,13 @@
 output "ssh_gateway_a" {
   value = <<-EOT
-    ssh -i ${path.module}/.key/keypair.pem ubuntu@${aws_instance.gateway["onpremises_gateway_ec2_a"].id} \
+    ssh -i ${path.module}/.key/keypair.pem ubuntu@${aws_instance.gateway_a.id} \
+    -o ProxyCommand="aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p' --profile admin"
+  EOT
+}
+
+output "ssh_gateway_c" {
+  value = <<-EOT
+    ssh -i ${path.module}/.key/keypair.pem ubuntu@${aws_instance.gateway_c.id} \
     -o ProxyCommand="aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p' --profile admin"
   EOT
 }
